8504f830ba8b3fd6554df54e6370c43b64039a45
gustavo-vasquez <vasquez.mgustavo@gmail.com>
2022-09-13 16:19:34 -0300

    Agregado: grilla mock de posteos con una barra de pestañas.

--- src/components/user/Profile.jsx
+++ src/components/user/Profile.jsx
@@ -5,6 +5,16 @@ import {Search} from 'components/Search';
 export const Profile = (props) => {
 	const location = useLocation();
 	let {username, email, profilePicture} = location.state;
+
+	function thumbnail_loop() {
+		let thumbs = [];
+		for(let i=0;i<12;i++) {
+			thumbs.push(<div className='col-md-3' key={i}>
+			<img className='img-fluid' src='https://dummyimage.com/600x400/000/fff' alt='post_thumbnail'/>
+		</div>);
+		}
+		return thumbs;
+	}
 	
 	return (
 		<div>
@@ -55,7 +65,32 @@ export const Profile = (props) => {
 					<Link to="/" className="d-block">https://www.google.com.ar</Link>
 					<Link to="/" className="d-block">https://www.google.com.ar</Link>
 				</div>
-				<div className="col-12 col-md-8"></div>
+				<div className="col-12 col-md-8">
+					<ul className="nav nav-pills justify-content-center" id="pills-tab" role="tablist">
+						<li className="nav-item" role="presentation">
+							<button className="nav-link active" id="pills-home-tab" data-bs-toggle="pill" data-bs-target="#pills-home" type="button" role="tab" aria-controls="pills-home" aria-selected="true">Fotos y videos</button>
+						</li>
+						<li className="nav-item" role="presentation">
+							<button className="nav-link" id="pills-profile-tab" data-bs-toggle="pill" data-bs-target="#pills-profile" type="button" role="tab" aria-controls="pills-profile" aria-selected="false">Fotos</button>
+						</li>
+						<li className="nav-item" role="presentation">
+							<button className="nav-link" id="pills-contact-tab" data-bs-toggle="pill" data-bs-target="#pills-contact" type="button" role="tab" aria-controls="pills-contact" aria-selected="false">Videos</button>
+						</li>
+						<li className="nav-item" role="presentation">
+							<button className="nav-link" id="pills-disabled-tab" data-bs-toggle="pill" data-bs-target="#pills-disabled" type="button" role="tab" aria-controls="pills-disabled" aria-selected="false" disabled>Disabled</button>
+						</li>
+					</ul>
+					<div className="tab-content" id="pills-tabContent">
+						<div className="tab-pane fade show active" id="pills-home" role="tabpanel" aria-labelledby="pills-home-tab" tabIndex="0">
+							<div className='row post-thumbnails'>
+								{thumbnail_loop()}
+							</div>
+						</div>
+						<div className="tab-pane fade" id="pills-profile" role="tabpanel" aria-labelledby="pills-profile-tab" tabIndex="0">...</div>
+						<div className="tab-pane fade" id="pills-contact" role="tabpanel" aria-labelledby="pills-contact-tab" tabIndex="0">...</div>
+						<div className="tab-pane fade" id="pills-disabled" role="tabpanel" aria-labelledby="pills-disabled-tab" tabIndex="0">...</div>
+					</div>
+				</div>
 			</div>
 			{/*<h1>Datos obtenidos:</h1>
 			<p>Access token: {location.state.accessToken}</p>
